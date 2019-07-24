db0c547ee7df3215af04e15de252cdd086bc7c95
Yurii Zherebukh <yura7887@gmail.com>
2019-07-24 13:21:33 +0300

    Adding setup.py and config files for setuptools. Removing base project and leaving easy_login app

--- easy_login/context_processors.py
+++ easy_login/context_processors.py
@@ -7,6 +7,6 @@ def easy_login(request):
     context = {}
     context.update(csrf(request))
     context['form'] = SwitchUserForm(initial={'user': request.user.id})
-    render_page = render_to_string("base.html", context)
+    render_page = render_to_string("easy_login_form.html", context)
 
     return {"easy_login": render_page}
