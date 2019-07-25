12369c86b749efff091757c687c61e775f0f4a89
Oleh-Hrebchuk <oleh.hrebchuk@gmail.com>
2019-07-25 17:09:36 +0300

    test
    test

--- easy_login/context_processors.py
+++ easy_login/context_processors.py
@@ -8,5 +8,6 @@ def easy_login(request):
     context.update(csrf(request))
     context['form'] = SwitchUserForm(initial={'user': request.user.id})
     render_page = render_to_string("base.html", context)
+    print('render_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_pagerender_page')
 
     return {"easy_login": render_page}
