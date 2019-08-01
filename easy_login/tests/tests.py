92dcd4ea65f9d58c9aede5c7a2fdc2e660a13698
onakonechny <onakonechny@deloitte.com>
2019-08-01 17:04:51 +0300

    Tests fixes from PR

--- easy_login/tests/tests.py
+++ easy_login/tests/tests.py
@@ -1,61 +1,74 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import factory
 from django.contrib.auth.models import User
 from django.test import RequestFactory, TestCase, Client
 from bs4 import BeautifulSoup
+from django.urls import reverse_lazy
+
 from easy_login.context_processors import easy_login
 
 import re
 
 
+class AdminFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        django_get_or_create = ('username', )
+
+    username = 'Admin'
+    email = 'admin@adm.com'
+    is_staff = True
+    is_superuser = True
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+        django_get_or_create = ('username', )
+
+    username = 'User'
+    email = 'admin@adm.com'
+    is_staff = True
+    is_superuser = True
+
+
 class EasyLoginTest(TestCase):
     def setUp(self):
-        self.factory = RequestFactory()
-
-        self.admin_username = "Admin"
         self.admin_password = "admin"
-
-        self.user_1_username = "User_1"
         self.user_1_password = "user1"
 
-        self.admin = User.objects.create_user(
-            username=self.admin_username,
-            password=self.admin_password,
-            email='admin@adm.com',
-            is_superuser=True,
-            is_staff=True
-        )
-        self.user_1 = User.objects.create_user(
-            username='User_1',
-            password='user1',
-            email='user1@adm.com'
-        )
-        self.user_2 = User.objects.create_user(
-            username='User_2',
-            password='user2',
-            email='user2@adm.com'
-        )
-        self.user_3 = User.objects.create_user(
-            username='User_3 long name test',
-            password='user3',
-            email='user3@adm.com'
-        )
+        self.admin = AdminFactory()
+        self.admin.set_password(self.admin_password)
+        self.admin.save()
+
+        self.user_1 = UserFactory()
+        self.user_1.set_password(self.user_1_password)
+        self.user_1.save()
+
+        self.user_2 = UserFactory(username='User_2')
+        self.user_2.set_password('user2')
+        self.user_2.save()
+
+        self.user_3 = UserFactory(username='User_3')
+        self.user_3.set_password('user3')
+        self.user_3.save()
 
     def test_user_list_view(self):
-        request = self.factory.get('/')
+        request_factory = RequestFactory()
+        request = request_factory.get(reverse_lazy('test-app:index'))
         request.user = self.admin
         response = easy_login(request)
-        with open('template.html', 'w') as ff:
-            ff.write(response['easy_login'])
         soup = BeautifulSoup(response['easy_login'], features="html.parser")
         user_name_obj = soup.find('select', {'id': 'id_user_name'})
-        user_real_list = set(User.objects.all().values_list('username', flat=True))
-        user_new_list = {user_obj.text for user_obj in user_name_obj.find_all('option') if user_obj['value']}
-        self.assertEqual(user_new_list, user_real_list)
+        user_real_set = set(User.objects.all().values_list('username', flat=True))
+        user_new_set = {user_obj.text for user_obj in user_name_obj.find_all('option') if user_obj['value']}
+        self.assertEqual(user_new_set, user_real_set)
 
     def test_current_user(self):
-        request = self.factory.get('/')
+        request_factory = RequestFactory()
+        request = request_factory.get(reverse_lazy('test-app:index'))
         for user in [self.admin, self.user_1, self.user_2, self.user_3]:
             request.user = user
             response = easy_login(request)
@@ -67,44 +80,43 @@ class EasyLoginTest(TestCase):
 
     def test_form(self):
         client = Client()
-        client.login(username=self.admin_username, password=self.admin_password)
+        client.login(username=self.admin.username, password=self.admin_password)
 
-        response = client.get('/test_app/')
+        response = client.get(reverse_lazy('test-app:index'))
         soup = BeautifulSoup(response.content, features="html.parser")
         search_compile = re.compile('Current User: (.*) - ID:.*')
         user_name = soup.find('p', text=search_compile)
         user_name = re.findall(search_compile, user_name.text)[0] if user_name else None
-
-        self.assertEqual('Admin', user_name)
+        self.assertEqual(self.admin.username, user_name)
 
         client.post(
-            '/easy_login/easy_login_change/',
+            reverse_lazy('easy-login:easy-login-change'),
             {
                 'user_name': '',
                 'user_id': self.user_1.id
             }
         )
 
-        response = client.get('/test_app/')
+        response = client.get(reverse_lazy('test-app:index'))
         soup = BeautifulSoup(response.content, features="html.parser")
         search_compile = re.compile('Current User: (.*) - ID:.*')
         user_name = soup.find('p', text=search_compile)
         user_name = re.findall(search_compile, user_name.text)[0] if user_name else None
 
-        self.assertEqual(self.user_1_username, user_name)
+        self.assertEqual(self.user_1.username, user_name)
 
         client.post(
-            '/easy_login/easy_login_change/',
+            reverse_lazy('easy-login:easy-login-change'),
             {
                 'user_name': self.admin.id,
                 'user_id': ''
             }
         )
 
-        response = client.get('/test_app/')
+        response = client.get(reverse_lazy('test-app:index'))
         soup = BeautifulSoup(response.content, features="html.parser")
         search_compile = re.compile('Current User: (.*) - ID:.*')
         user_name = soup.find('p', text=search_compile)
         user_name = re.findall(search_compile, user_name.text)[0] if user_name else None
 
-        self.assertEqual(self.admin_username, user_name)
+        self.assertEqual(self.admin.username, user_name)
