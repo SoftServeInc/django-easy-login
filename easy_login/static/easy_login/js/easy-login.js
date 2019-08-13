387ada97443aa185b7bf6cb885e541dad9649959
onakonechny <onakonechny@deloitte.com>
2019-08-13 13:50:21 +0300

    Tests fixes from PR (2)

--- easy_login/static/easy_login/js/easy-login.js
+++ easy_login/static/easy_login/js/easy-login.js
@@ -1,19 +1,37 @@
-function ToggleEasyLoginMenu() {
-    var toggleClassNames = ['easy-login-detail-info', 'easy-login-toggle'];
-    toggleClassNames.forEach(function (className, index) {
-        var elements = document.getElementsByClassName(className);
-        for (var i = 0, l = elements.length; i < l; i++) {
-            var obj = elements[i];
-            if (obj.style.display === "none") {
-                obj.style.display = "";
-            } else {
-                obj.style.display = "none";
-            }
+Element.prototype.hasClass = function(className) {
+    return new RegExp(' ' + className + ' ').test(' ' + this.className + ' ');
+};
+Element.prototype.addClass = function(className) {
+    if (!this.hasClass(className)) {
+        this.className += ' ' + className;
+    }
+};
+Element.prototype.removeClass = function(className) {
+    var newClass = ' ' + this.className.replace(/[\t\r\n]/g, ' ') + ' '
+    if (this.hasClass(className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
         }
-        });
-}
-
-
+        this.className = newClass.replace(/^\s+|\s+$/g, ' ');
+    }
+};
+Element.prototype.toggleClass = function(className) {
+    var newClass = ' ' + this.className.replace(/[\t\r\n]/g, " ") + ' ';
+    if (this.hasClass(className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0) {
+            newClass = newClass.replace(" " + className + " ", " ");
+        }
+        this.className = newClass.replace(/^\s+|\s+$/g, ' ');
+    } else {
+        this.className += ' ' + className;
+    }
+};
+var EasyLogin = {};
+EasyLogin.on_image_click = function() {
+    var elem = document.getElementById('easy_login_box');
+    elem.toggleClass("hidden_box");
+    elem.toggleClass("visible_box");
+};
 document.getElementById('id_user_name').onchange = function() {
-    document.getElementById('easy-login-form').submit();
+    document.getElementById('easy_login_form').submit();
 };
