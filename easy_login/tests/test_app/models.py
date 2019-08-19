73381cab75efdc45d87040b0c27ca35ac864c253
Matiiash, Vasyl (US - Morrisville) <vamatiiash>
2019-08-19 12:23:40 +0300

    Resolved issues founded by  Codacy

--- easy_login/tests/test_app/models.py
+++ easy_login/tests/test_app/models.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models
-from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin, BaseUserManager)
-from django.utils import timezone
+# from django.db import models
+# from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin, BaseUserManager)
+# from django.utils import timezone
 
 
 # class UserManager(BaseUserManager):
